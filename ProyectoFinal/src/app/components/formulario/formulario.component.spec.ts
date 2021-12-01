2d5aa0e2f277d9b1360dbdc81fdd0f630b0c0e85
NoemiHMartin <83716758+noemiepsilon@users.noreply.github.com>
2021-12-01 15:50:28 +0100

    Guardando datos para pull

--- ProyectoFinal/src/app/components/formulario/formulario.component.spec.ts
+++ ProyectoFinal/src/app/components/formulario/formulario.component.spec.ts
@@ -1,15 +1,20 @@
-//TEST DE BDD
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+// //TEST DE BDD
+import { ComponentFixture} from '@angular/core/testing';
+import { ReactiveFormsModule, FormBuilder } from '@angular/forms';
+import { inject, TestBed } from '@angular/core/testing';
+import { FormsModule} from '@angular/forms';
 import { FormularioComponent } from './formulario.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('FormularioComponent', () => {
   let component: FormularioComponent;
   let fixture: ComponentFixture<FormularioComponent>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ FormularioComponent ]
+    TestBed.configureTestingModule({
+      declarations: [ FormularioComponent ],
+      imports: [ ReactiveFormsModule, FormsModule, HttpClientTestingModule ],
+      providers: [{provide: FormularioComponent}],
     })
     .compileComponents();
   });
@@ -20,7 +25,14 @@ describe('FormularioComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should be created', inject([FormularioComponent], (service: FormularioComponent) => {
+    expect(service).not.toBeNull();
+  }));
+
+
+  it('should detect form is valid', () => {
+    fixture.nativeElement.querySelector('button').click();
+
+    expect(component.agregarDatosPersonales()).toEqual();
   });
-});
+  });
