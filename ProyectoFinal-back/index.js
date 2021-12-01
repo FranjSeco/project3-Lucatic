a586b48cdf2da8e5bccb6d34c517b190bd482067
NoemiHMartin <83716758+noemiepsilon@users.noreply.github.com>
2021-12-01 10:38:12 +0100

    Guardando para pull

--- ProyectoFinal-back/index.js
+++ ProyectoFinal-back/index.js
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-import express from "express";
-
-import mongoose from "mongoose";
-
-import { mockUsers } from "./mockUsers/mockUsers.js";
-import userRoute from "./routes/users.js";
-
-const app = express();
-
-import cors from "cors";
-import bodyParser from "body-parser";
-
-mongoose.connect("mongodb://localhost:27017/proyectoFinal", {
-=======
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
@@ -26,7 +11,6 @@ mongoose.Promise = global.Promise;
 let dbData = user;
 
 mongoose.connect('mongodb://localhost:27017/proyectoFinal', {
->>>>>>> 523c5388bf5638f60a3d0b74337ce1fe6c2d4e69
   useNewUrlParser: true,
   //useFindAndModify: false,
   useUnifiedTopology: true
@@ -48,27 +32,6 @@ app.use(bodyParser.urlencoded({
   extended: false
 }));
 
-<<<<<<< HEAD
-app.post("/register", userRoute);
-
-// mockUsers.map((item) => {
-//   createUser({
-//     name: item.user,
-//     genero: item.genero,
-//     email: item.email,
-//     password: item.password,
-//   });
-// });
-
-app.listen(8080, () => {
-  console.log("Escuchando por el puerto 8080");
-});
-
-app.get("/register", (req, res) => {
-  res.send("invalid endpoint");
-  console.log(res);
-});
-=======
 ///seguridad; permite accesder a servidores de terceros; elegir proveedores en especifico por ej
 app.use(cors());
 
@@ -124,4 +87,3 @@ app.use(function (err, req, res, next) {
   if (!err.statusCode) err.statusCode = 500;
   res.status(err.statusCode).send(err.message);
 });
->>>>>>> 523c5388bf5638f60a3d0b74337ce1fe6c2d4e69
