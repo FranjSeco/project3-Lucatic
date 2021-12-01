fef1aeff026d5d661a665990d78e405183ead130
Daniel <tennluis32@gmail.com>
2021-12-01 08:13:09 +0100

    hola

--- ProyectoFinal/src/app/app-routing.module.ts
+++ ProyectoFinal/src/app/app-routing.module.ts
@@ -4,15 +4,11 @@ import { AppComponent } from './app.component';
 import { RegisterComponent } from './components/register/register.component';
 import { DisplayComponent } from './components/display/display.component';
 
-<<<<<<< HEAD
 const routes: Routes = [
   { path: '', component: AppComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'display', component: DisplayComponent },
 ];
-=======
-const routes: Routes = [{ path: 'register', component: RegisterComponent }];
->>>>>>> 523c5388bf5638f60a3d0b74337ce1fe6c2d4e69
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ProyectoFinal/src/app/components/register/register.component.ts
+++ ProyectoFinal/src/app/components/register/register.component.ts
@@ -10,29 +10,6 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
-<<<<<<< HEAD
-  @Output() onAddUser: EventEmitter<User> = new EventEmitter();
-  name!: string;
-  genero!: string;
-  email!: string;
-  password!: number;
-  constructor(private auth: AuthService) {}
-
-  ngOnInit(): void {}
-
-  onSubmit() {
-    if (!this.name) {
-      alert('Please add name');
-      return;
-    }
-
-    const newUser = {
-      name: this.name,
-      genero: this.genero,
-      email: this.email,
-      password: 1,
-    };
-=======
   usuario!: UserInterface;
   constructor(
     private auth: AuthService,
@@ -41,7 +18,6 @@ export class RegisterComponent implements OnInit {
   ) {
     this.usuario = {} as UserInterface;
   }
->>>>>>> 523c5388bf5638f60a3d0b74337ce1fe6c2d4e69
 
   ngOnInit(): void {
     this.usuario = {} as UserInterface;
