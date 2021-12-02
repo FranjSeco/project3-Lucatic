f2b48c71e4a98940f082d09bdd640b3498b8ce62
Daniel <tennluis32@gmail.com>
2021-12-02 11:56:26 +0100

    animacion el nav de los perfiles

--- ProyectoFinal/src/app/components/display/display.component.ts
+++ ProyectoFinal/src/app/components/display/display.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { TakeUsersService } from 'src/app/services/take-users.service';
 import { NgZone } from '@angular/core';
 import { Router } from '@angular/router';
+import { first } from 'rxjs';
 @Component({
   selector: 'app-display',
   templateUrl: './display.component.html',
@@ -19,10 +20,62 @@ export class DisplayComponent implements OnInit {
     this.getAllUsers();
   }
 
-  switchWindow() {
+  switchWindow(window: string) {
     console.log(event?.target);
-    let option = <HTMLElement>document.getElementById('matches');
-    option.style.borderBottom = '4px solid #FF5B6C';
+    let option = document.getElementById(window);
+    let firstHr = <HTMLElement>document.getElementById('iMatch');
+    let secondHr = <HTMLElement>document.getElementById('iMess');
+    let thirdHr = <HTMLElement>document.getElementById('iLike');
+    let fourthHr = <HTMLElement>document.getElementById('iDis');
+    let hr = <HTMLElement>option?.children.item(1);
+
+    switch (window) {
+      case 'matches':
+        secondHr.style.opacity = '0';
+        thirdHr.style.opacity = '0';
+        fourthHr.style.opacity = '0';
+        secondHr.style.translate = '-120%';
+        thirdHr.style.translate = '-375%';
+        fourthHr.style.translate = '-400%';
+        hr.style.translate = '0';
+        hr.style.opacity = '1';
+        break;
+      case 'messages':
+        firstHr.style.opacity = '0';
+        thirdHr.style.opacity = '0';
+        fourthHr.style.opacity = '0';
+        firstHr.style.translate = '140%';
+        thirdHr.style.translate = '-180%';
+        fourthHr.style.translate = '-253%';
+        hr.style.translate = '0';
+        hr.style.opacity = '1';
+        break;
+      case 'likes':
+        firstHr.style.opacity = '0';
+        secondHr.style.opacity = '0';
+        fourthHr.style.opacity = '0';
+        firstHr.style.translate = '250%';
+        secondHr.style.translate = '100%';
+        fourthHr.style.translate = '-120%';
+        hr.style.translate = '0';
+        hr.style.opacity = '1';
+        break;
+      case 'dislikes':
+        firstHr.style.opacity = '0';
+        secondHr.style.opacity = '0';
+        thirdHr.style.opacity = '0';
+        firstHr.style.translate = '375%';
+        secondHr.style.translate = '200%';
+        thirdHr.style.translate = '160%';
+        hr.style.translate = '0';
+        hr.style.opacity = '1';
+
+        break;
+
+      default:
+        break;
+    }
+    let options = ['matches', 'messages', ''];
   }
 
   getAllUsers() {
